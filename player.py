4e90ec118973354f7f3d809c29a2c05f5d1991f8
JuliaBatmanova <yubatmanova@yandex.ru>
2026-03-27 10:04:56 +0500

    Update player.py

--- player.py
+++ player.py
@@ -1,32 +1,49 @@
 import pygame
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
 class Player:
+
     def __init__(self, x, y):
+
         self.rect = pygame.Rect(x, y, 40, 40)
+
+        self.color = (0, 255, 0)
+
         self.speed = 5
+
         self.score = 0
 
     def move(self, keys):
-        if keys[pygame.K_LEFT] and self.rect.left > 0:
+
+        if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
 
-        if keys[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
+        if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
 
-        if keys[pygame.K_UP] and self.rect.top > 0:
+        if keys[pygame.K_UP]:
             self.rect.y -= self.speed
 
-        if keys[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
+        if keys[pygame.K_DOWN]:
             self.rect.y += self.speed
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, (0,128,255), self.rect)
+        # Ограничение по экрану
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        if self.rect.right > 800:
+            self.rect.right = 800
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+        if self.rect.bottom > 600:
+            self.rect.bottom = 600
 
     def collect_point(self):
+
         self.score += 10
 
-    def reset_score(self):
-        self.score = 0
+    def draw(self, screen):
+
+        pygame.draw.rect(screen, self.color, self.rect)
